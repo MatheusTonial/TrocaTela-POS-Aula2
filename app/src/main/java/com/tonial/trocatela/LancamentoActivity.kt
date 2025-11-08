3ec4babb6846c3f638e856540fe7e7637df6b6d2
Matheus <matheustonial97@gmail.com>
2025-11-08 13:56:07 -0300

    feat: Add Lancamento activity and navigation
    Adds a new `LancamentoActivity` with a corresponding layout for data entry, including fields for code, quantity, and value.
    
    The `MainActivity` is updated to navigate to this new activity when the "Lançamento" button is clicked. The new activity is also registered in the `AndroidManifest.xml`.

--- app/src/main/java/com/tonial/trocatela/LancamentoActivity.kt
+++ app/src/main/java/com/tonial/trocatela/LancamentoActivity.kt
@@ -1,21 +1,18 @@
 package com.tonial.trocatela
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LancamentoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lancamento)
 
     }
 
-    fun btnLancamentoOnClick(view: View) {
-        val intent = Intent(this, LancamentoActivity::class.java)
-        startActivity(intent)
-    }
+    fun btnConfirmarClick(view: View) {}
+    fun btnListarClick(view: View) {}
 }
