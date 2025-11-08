428ca198c0b3c5433568ffeac0579814266a491c
Matheus <matheustonial97@gmail.com>
2025-11-08 14:35:15 -0300

    feat: Add Confirmar activity and data passing
    Introduces `ConfirmarActivity` to display data entered in `LancamentoActivity`.
    
    The `LancamentoActivity` now reads values from its input fields and passes them as extras in an Intent to the new `ConfirmarActivity` when the "Confirmar" button is clicked.
    
    The new `ConfirmarActivity` retrieves these extras and displays them in TextViews. It includes a layout with a "Confirmar com SMS" button. The new activity is also registered in the `AndroidManifest.xml`.

--- app/src/main/java/com/tonial/trocatela/LancamentoActivity.kt
+++ app/src/main/java/com/tonial/trocatela/LancamentoActivity.kt
@@ -1,18 +1,35 @@
 package com.tonial.trocatela
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class LancamentoActivity : AppCompatActivity() {
+
+    private lateinit var etCod: EditText
+    private lateinit var etQtd: EditText
+    private lateinit var etValor: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lancamento)
 
+        etCod = findViewById(R.id.etCod)
+        etQtd = findViewById(R.id.etQtd)
+        etValor = findViewById(R.id.etValor)
     }
 
-    fun btnConfirmarClick(view: View) {}
+    fun btnConfirmarClick(view: View) {
+        val intent = Intent(this, ConfirmarActivity::class.java)
+        intent.putExtra("cod", etCod.text.toString().toIntOrNull())
+        intent.putExtra("qtd", etQtd.text.toString())
+        intent.putExtra("valor", etValor.text.toString())
+        startActivity(intent)
+    }
+
     fun btnListarClick(view: View) {}
 }
